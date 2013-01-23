173f64b809216ee431fb3cece5391618ef6aa09a
priyanka1-webonise <ukirde.priyanka@webiniselab.com>
2013-01-23 12:31:06 +0530

    first commit

---
+++ devise_assignment/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeviseAssignment::Application.config.secret_token = '3800d8c01c4b78ac697cc74c103824bc17224213934b678cc9743e4f41a8b0b8f6d17c8bcdd11ad5f18663a35b34b62cddc7c79766d626d417fcbecebf981eb9'
